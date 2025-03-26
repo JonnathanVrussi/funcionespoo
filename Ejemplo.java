37a12f3d35e0eac0bc2fba68e8961fb2cf1d2c33
JonnathanVrussi <jonnathan.velasquez@teinco.edu.co>
2025-03-26 10:33:35 -0500

    Add files via upload

---
+++ Ejemplo.java
@@ -0,0 +1,16 @@
+// Creación de la Función sumar dos npumeros en Java 
+public class Ejemplo {
+    
+    // Creación de la función 
+ 
+    public static int sumar(int a , int b) {
+        return a + b ;
+       
+    }
+
+    // Ejecución de la función sumar
+    public static void main(String[] args) {
+        int resultado = sumar (15, 333);
+        System.out.println(resultado);
+    }
+}
